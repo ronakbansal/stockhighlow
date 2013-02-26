8203d7c6002c2164a42e93b55b11603fa18bdfbd
Ronak Bansal <Ronak Bansal@.ads.mckinsey.com>
2013-02-26 16:16:57 +0530

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,32 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'fastercsv'
+class WelcomeController < ApplicationController
+  def index
+    
+  end
+
+  def search
+    doc = Nokogiri::HTML(open("http://d.yimg.com/aq/autoc?query=#{params[:q]}&region=IN&lang=en-IN&callback=YAHOO.util.ScriptNodeDataSource.callbacks"))
+    text = doc.text
+    actual_text = text[/#{Regexp.escape('[')}(.*?)#{Regexp.escape(']')}/m, 1]
+    split_text = actual_text.split(/},/)
+    @new_arr = []
+    split_text.each_with_index do |x,index|
+      x = x.to_s + '}' unless split_text.length == index+1
+      x = JSON.parse(x)
+      @new_arr << x
+    end
+    render :partial => "search"
+  end
+
+  def find_requested_data
+    from_month,from_day,from_year = params[:from_date].split('/').map(&:to_i)
+    from_month = from_month - 1
+    to_month,to_day,to_year = params[:to_date].split('/').map(&:to_i)
+    to_month = to_month - 1
+    @csv = FasterCSV.parse(open("http://ichart.yahoo.com/table.csv?s=#{params[:symbol]}&a=#{from_month}&b=#{from_day}&c=#{from_year}&d=#{to_month}&e=#{to_day}&f=#{to_year}&g=d&ignore=.csv"),{:headers => true,:return_headers => false})
+    render :partial => "find_requested_data"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockHighLow::Application.config.secret_token = 'e7984dc2f81558627df63edd6cb2bce8c91d2e5117bebf909e1d2c51fd879de4c599478aff6599a53a62691b699d701b1535ada0fefa089158ba7704bada8c93'
