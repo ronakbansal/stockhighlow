79a3a369854413a4a0a6bfe7086fc8b9201c5eff
Ronak Bansal <Ronak Bansal@.ads.mckinsey.com>
2013-02-26 19:50:28 +0530

    finding whether accessing from mobile or web

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+
+def mobile_device?
+  if session[:mobile_param]
+    session[:mobile_param] == "1"
+  else
+    request.user_agent =~ /Mobile|webOS/
+  end
+end
+helper_method :mobile_device?
 end
